4fde81d3c92b8f223fecf0207aa872052b3ea96e
Nicholas Miller <nicholasmill157@gmail.com>
2022-11-03 18:56:43 -0400

    working on schedule function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $("#currentDay").text(currentDay);
 //console.log(currentDay);
 //console.log(currentTime);
 
-$(".hour")
+
 
 $(document).ready(function() {
 
@@ -14,33 +14,45 @@ $(document).ready(function() {
     // Function to handle Time for the schedule
     function scheduleTimer() {
         
-        var hour = parseInt($("hour").attr("id").split("hour")[1]);
-
-        //Background color if statement for each scenerio
-        if ( hour == currentTime) {
-            $(this).addClass("present");
-            $(this).removeClass("past");
-                
-        } else if (hour > currentTime) {
-            $(this).removeClass("present");
-            $(this).addClass("future");
+        $(".hour").each(function () {
+            var timeDiv = parseInt($('.hour').attr("id").split("hour")[1]);
+            
+            //Background color if statement for each scenerio
+            if (timeDiv == currentTime) {
+              $(this).addClass("present");
+              $(this).removeClass("past");
+            
+            } else if (timeDiv > currentTime) {
+              $(this).removeClass("present");
+              $(this).addClass("future");
+    
+            } else if (timeDiv < currentTime) {
+              $(this).removeClass("future");
+              $(this).addClass("past");
+            }
+    
+          });
         
-        } else if (hour < currentTime) {
-            $(this).removeClass("future");
-            $(this).addClass("past");
-        }
             
     }
         
-        scheduleTimer();
-        
-        // Save button Function
-        $('saveBtn').on("click", function() {
-            var desc = $(this).siblings(".description").val();
-            var time = $(this).parent().attr("id");
-        
-            localStorage.setItem(time, desc);
-        })
+    // Save button Function
+$('saveBtn').on("click", function() {
+    var desc = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-        
+    localStorage.setItem(time, desc);
+})
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
 })
