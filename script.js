0ef8d689fda7e718fe31d45ad486e13fb755dcd7
Nicholas Miller <nicholasmill157@gmail.com>
2022-11-03 16:33:38 -0400

    HTML set and javascript created

---
+++ script.js
@@ -0,0 +1,10 @@
+//Creating time variable and Setting Date on display
+var currentDay = moment().format("ddd MMM Do, YYYY");
+var currentTime = moment().format("HH");
+$("#currentDay").text(currentDay);
+
+
+// Function to handle Time for the schedule
+
+// Save button Function
+ 
