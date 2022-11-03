9eb7b0b9f88b8b44b74342ccf628564283b49f85
Nicholas Miller <nicholasmill157@gmail.com>
2022-11-03 18:21:11 -0400

    figuring out schedule function

--- script.js
+++ script.js
@@ -6,24 +6,41 @@ $("#currentDay").text(currentDay);
 //console.log(currentDay);
 //console.log(currentTime);
 
+$(".hour")
+
 $(document).ready(function() {
 
 
     // Function to handle Time for the schedule
     function scheduleTimer() {
         
+        var hour = parseInt($("hour").attr("id").split("hour")[1]);
+
+        //Background color if statement for each scenerio
+        if ( hour == currentTime) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+                
+        } else if (hour > currentTime) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
         
-        
+        } else if (hour < currentTime) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+            
     }
         
-    scheduleTimer();
+        scheduleTimer();
         
-    // Save button Function
-    $('saveBtn').on("click", function() {
-        var desc = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        // Save button Function
+        $('saveBtn').on("click", function() {
+            var desc = $(this).siblings(".description").val();
+            var time = $(this).parent().attr("id");
         
-        localStorage.setItem(time, desc);
-    })
+            localStorage.setItem(time, desc);
+        })
+
         
 })
